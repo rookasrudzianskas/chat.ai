cc4261f293123a3a09d1941df507a7343133e906
Rokas <rookas.rudzenskas@gmail.com>
2023-08-04 12:51:44 +0300

    FIX

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    experimental: {
+        serverActions: true
+    },
+    images: {
+        domains: [
+            "res.cloudinary.com",
+        ],
+    },
+};
 
 module.exports = nextConfig
