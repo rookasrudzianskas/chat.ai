7988bcfc266959365fd941428e857442e873ce93
Rokas <rookas.rudzenskas@gmail.com>
2023-08-04 10:52:35 +0300

    FIX

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,7 @@
-// import { Sidebar } from "@/components/sidebar";
 // import { checkSubscription } from "@/lib/subscription";
 
 import Navbar from "@/components/navbar";
+import Sidebar from "@/components/sidebar";
 
 const RootLayout = async ({
   children
@@ -14,7 +14,7 @@ const RootLayout = async ({
     <div className="h-full">
       <Navbar />
       <div className="hidden md:flex mt-16 h-full w-20 flex-col fixed inset-y-0">
-        {/*<Sidebar isPro={isPro} />*/}
+        <Sidebar />
       </div>
       <main className="md:pl-20 pt-16 h-full">
         {children}
