902cdbe268f5887b714e8b0fbbaef3e74114a6a2
Rokas <rookas.rudzenskas@gmail.com>
2023-08-04 10:42:18 +0300

    FIX

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <html lang="en">
+      <html lang="en" suppressHydrationWarning>
         <body className={inter.className}>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             {children}
