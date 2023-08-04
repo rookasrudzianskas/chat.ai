d7c86e10f7d6964d51927f9e723b5a89f48d6a58
Rokas <rookas.rudzenskas@gmail.com>
2023-08-04 10:57:32 +0300

    FIX

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -9,6 +9,7 @@ import {UserButton} from "@clerk/nextjs";
 import {cn} from "@/lib/utils";
 import {Button} from "@/components/ui/button";
 import {ModeToggle} from "@/components/mode-toggle";
+import {MobileSidebar} from "@/components/mobile-sidebar";
 
 const font = Poppins({ weight: "600", subsets: ["latin"] });
 
@@ -16,7 +17,7 @@ const Navbar = ({}) => {
   return (
     <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 h-16 border-b border-primary/10 bg-secondary">
       <div className="flex items-center">
-        <Menu className="block md:hidden" />
+        <MobileSidebar isPro={false} />
         <Link href={'/'}>
           <h1 className={cn(
             `hidden md:block text-xl md:text-3xl font-bold text-primary`,
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 import {Home, Plus, Settings} from "lucide-react";
 
 import {cn} from "@/lib/utils";
-import {usePathname} from "next/navigation";
+import {usePathname, useRouter} from "next/navigation";
 
 const routes = [
   {
@@ -29,6 +29,12 @@ const routes = [
 
 const Sidebar = ({}) => {
   const pathname = usePathname()
+  const router = useRouter()
+  const onNavigate = (url: string, pro: boolean) => {
+
+    return router.push(url)
+  }
+
   return (
     <div className="space-y-4 flex flex-col h-full text-primary bg-secondary">
       <div className="p-3 flex flex-1 justify-center">
@@ -36,6 +42,7 @@ const Sidebar = ({}) => {
           {routes.map((route) => (
             <div
               key={route.href}
+              onClick={() => onNavigate(route.href, route.pro)}
               className={cn(
                 "text-muted-foreground text-xs group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-primary hover:bg-primary/10 rounded-lg transition",
                 pathname === route.href && "bg-primary/10 text-primary",
