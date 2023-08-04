54a4e12668d288d6a50c5738c28c42a97eb01190
Rokas <rookas.rudzenskas@gmail.com>
2023-08-04 10:18:13 +0300

    FIX

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import {UserButton} from "@clerk/nextjs";
 
 const RootPage = ({}) => {
   return (
     <div>
-      
+      <UserButton afterSignOutUrl={'/'} />
     </div>
   );
 };
