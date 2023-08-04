d8b35fb8cd3b7dc145bcbab8c40b21c0b24c7b8e
Rokas <rookas.rudzenskas@gmail.com>
2023-08-04 11:39:22 +0300

    categories

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import SearchInput from "@/components/search-input";
+import prismadb from "@/lib/prismadb";
+import Categories from "@/components/categories";
 
-const RootPage = ({}) => {
+const RootPage = async ({}) => {
+  const categories = await prismadb.category.findMany();
+
   return (
     <div className="h-full p-4 space-y-2">
-      <SearchInput />
+      <SearchInput/>
+      <Categories data={categories} />
     </div>
   );
 };
