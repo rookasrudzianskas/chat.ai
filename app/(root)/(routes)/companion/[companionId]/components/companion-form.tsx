d209b8782ac6827ce142c7975dc06188fa84d9ba
Rokas <rookas.rudzenskas@gmail.com>
2023-08-04 12:13:07 +0300

    FIX

--- app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
+++ app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import * as z from "zod";
 import React from 'react';
 import {Category, Companion} from "@prisma/client";
 
@@ -7,6 +8,27 @@ interface CompanionFormProps {
   categories: Category[];
   initialData: Companion | null;
 };
+
+const formSchema = z.object({
+  name: z.string().min(1, {
+    message: "Name is required.",
+  }),
+  description: z.string().min(1, {
+    message: "Description is required.",
+  }),
+  instructions: z.string().min(200, {
+    message: "Instructions require at least 200 characters."
+  }),
+  seed: z.string().min(200, {
+    message: "Seed requires at least 200 characters."
+  }),
+  src: z.string().min(1, {
+    message: "Image is required."
+  }),
+  categoryId: z.string().min(1, {
+    message: "Category is required",
+  }),
+})
 
 const CompanionForm = ({
   categories,
